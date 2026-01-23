4b5d39d0ed14c0559020e4cd3bb76a7e02130fa9
basitGL <152172782+basitGL@users.noreply.github.com>
2026-01-23 20:22:03 +0500

    fix: exclude country code from surname

--- lib/src/mrz_parser/td3_format_mrz_parser.dart
+++ lib/src/mrz_parser/td3_format_mrz_parser.dart
@@ -97,11 +97,12 @@ class _TD3MRZFormatParser {
     log("sex: $sex");
     log("expiryDate: $expiryDate");
     log("optionalData: $optionalData");
+    final hasCountryCode = names[0].substring(0, 4) == countryCode;
 
     final result = MRZResult(
       documentType: documentType,
       countryCode: countryCode,
-      surnames: names[0],
+      surnames: hasCountryCode ? names[0].substring(4) : names[0],
       givenNames: names[1],
       documentNumber: documentNumber,
       nationalityCountryCode: nationality,
